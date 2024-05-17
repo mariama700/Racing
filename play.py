fd57a0ebf557ea56496aa9f9f1c734686d875f32
mariama700 <mariama70@nycstudents.net>
2024-05-17 08:23:05 -0400

    Update play.py

--- play.py
+++ play.py
@@ -1,1 +1,10 @@
+import pygame
 
+class Play:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("play_button.png")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
