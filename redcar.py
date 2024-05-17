f032256852b52db44e3fb5f2a699f446223ccad1
mariama700 <mariama70@nycstudents.net>
2024-05-17 08:28:56 -0400

    Update redcar.py

--- redcar.py
+++ redcar.py
@@ -1,1 +1,22 @@
+import pygame
 
+class RedCar:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("redcar.png")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = 3
+
+    def move_direction(self, direction):
+        if direction == "right":
+            self.x = self.x + self.delta
+        elif direction == "left":
+            self.x = self.x - self.delta
+        elif direction == "up":
+            self.y = self.y - self.delta
+        elif direction == "down":
+            self.y = self.y + self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
