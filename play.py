9a2d98a34dc7abb495172ddfdc109501b2af9fd7
mariama700 <mariama70@nycstudents.net>
2024-05-21 08:40:53 -0400

    Update play.py

--- play.py
+++ play.py
@@ -8,3 +8,7 @@ class Play:
         self.image = pygame.image.load("play_button.png")
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.image_size = self.image.get_size()
+        scale_size = (self.image_size[0] * .4, self.image_size[1] * .4)
+        self.image = pygame.transform.scale(self.image, scale_size)
+        self.image_size = self.image.get_size()
