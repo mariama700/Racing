b8e6a4eaf7d32cfcd85251912546eb4ce706888a
mariama700 <mariama70@nycstudents.net>
2024-06-07 08:40:42 -0400

    Update highway.py

--- highway.py
+++ highway.py
@@ -1,4 +1,7 @@
+#make rules slightly transparent
+#make popup show after 5 seconds
 import pygame
+
 
 class Highway:
   
@@ -9,3 +12,14 @@ class Highway:
     self.image_size = self.image.get_size()
     self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
 
+class Rules:
+  
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.image = pygame.image.load("rules.png")
+    self.image_size = self.image.get_size()
+    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+    scale_size = (self.image_size[0] * .62, self.image_size[1] * .62)
+    self.image = pygame.transform.scale(self.image, scale_size)
+    self.image_size = self.image.get_size()
