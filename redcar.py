470c64a86fa39829645a884f75b79109109c98e5
mariama700 <mariama70@nycstudents.net>
2024-06-18 05:48:13 -0400

    Update redcar.py

--- redcar.py
+++ redcar.py
@@ -2,21 +2,18 @@ import pygame
 
 class RedCar:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, scale_factor=0.7):
         self.x = x
         self.y = y
         self.image = pygame.image.load("redcar.png")
-        self.image_size = self.image.get_size()
-        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-        self.delta = 3
+        # Scale the car image
+        self.image = pygame.transform.scale(self.image, (int(self.image.get_width() * scale_factor), int(self.image.get_height() * scale_factor)))
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.delta = 1
 
     def move_direction(self, direction):
-        if direction == "right":
-            self.x = self.x + self.delta
-        elif direction == "left":
-            self.x = self.x - self.delta
-        elif direction == "up":
-            self.y = self.y - self.delta
-        elif direction == "down":
-            self.y = self.y + self.delta
-        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        if direction == "right" and self.x < 400:
+            self.x+=1
+        if direction == "left" and self.x > 100:
+            self.x-=1
+        self.rect.topleft = (self.x, self.y)
