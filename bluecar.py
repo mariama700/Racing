6ea0a5bc523a62c3f32fcaf5ec01c53745d2c588
mariama700 <mariama70@nycstudents.net>
2024-06-18 05:45:32 -0400

    Update bluecar.py

--- bluecar.py
+++ bluecar.py
@@ -1,1 +1,13 @@
+import pygame
 
+class BlueCar:
+
+    def __init__(self, x, y, scale_factor=0.7):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("blueCar.png")
+        # Scale the car image
+        self.image = pygame.transform.scale(self.image, (int(self.image.get_width() * scale_factor), int(self.image.get_height() * scale_factor)))
+        self.image = pygame.transform.rotate(self.image, 180)
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.delta = 1
